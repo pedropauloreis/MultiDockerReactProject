8c43f2281ce849810d2dff59b1cdf2c55e90e81d
Pedro Paulo de Oliveira Reis <ppreis@gmail.com>
2021-07-30 11:35:15 +0200

    k8s first file added

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1 >Fibonacci Calculator - Sample App with multi Docker instances</h1>
+          <h1 >Fibonacci Calculator - Sample App with multi Docker instances in K8S World</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
